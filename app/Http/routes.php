b3c2e9260b63f735adb57410ad8d542c0cf9829d
Bruno Silvagit config --global user.name Bruno <1111782@isep.ipp.pt>
2015-08-06 12:26:07 +0100

    Creating the Admin Area

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,23 @@ Route::get('/', function () {
 
 Route::get('blog','BlogController@index');
 Route::get('blog/{slug}','BlogController@showPost');
+
+// Admin area
+get('admin', function () {
+    return redirect('/admin/post');
+});
+$router->group([
+    'namespace' => 'Admin',
+    'middleware' => 'auth',
+], function () {
+    resource('admin/post', 'PostController');
+    resource('admin/tag', 'TagController');
+    get('admin/upload', 'UploadController@index');
+});
+
+// Logging in and out
+Route::get('/auth/login','Auth\AuthController@getLogin');
+Route::post('/auth/login','Auth\AuthController@postLogin');
+Route::get('/auth/register','Auth\AuthController@getRegister');
+Route::post('/auth/register','Auth\AuthController@postRegister');
+Route::get('/auth/logout','Auth\AuthController@getLogout');
