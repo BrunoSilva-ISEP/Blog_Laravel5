b9f7181975626723e83631ae6579589dba4436aa
Bruno Silvagit config --global user.name Bruno <1111782@isep.ipp.pt>
2015-08-10 14:02:09 +0100

    Creating the Comments, RSS, and a Site Map

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -42,7 +42,30 @@ class BlogController extends Controller
             $tag = Tag::whereTag($tag)->firstOrFail();
         }
 
-        return view($post->layout, compact('post', 'tag'));
+        return view($post->layout, compact('post', 'tag','slug'));
     }
 
+    /**
+     * @param RssFeed $feed
+     * @return mixed
+     */
+    public function rss(RssFeed $feed)
+    {
+        $rss = $feed->getRSS();
+
+        return response($rss)
+            ->header('Content-type', 'application/rss+xml');
+    }
+
+    /**
+     * @param SiteMap $siteMap
+     * @return mixed
+     */
+    public function siteMap(SiteMap $siteMap)
+    {
+        $map = $siteMap->getSiteMap();
+
+        return response($map)
+            ->header('Content-type', 'text/xml');
+    }
 }
